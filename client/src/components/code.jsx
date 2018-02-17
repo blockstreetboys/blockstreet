7889357e1e389481960d99356ea8e302e1f008c8
Daniel Nolan <DanNolan99@gmail.com>
2018-02-17 15:24:09 -0700

    code editor stuff

--- client/src/components/code.jsx
+++ client/src/components/code.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import CodeButtons from './code_buttons';
+import CodeEditor from './code_editor';
+import CodeDisplay from './code_display';
 
 class Code extends Component {
 
   render() {
     return (
       <div className='dev-code'>
-        <main>
-          im an embedded text editor
-        </main>
+        <CodeEditor />
+        <CodeDisplay />
         <CodeButtons />
       </div>
 
