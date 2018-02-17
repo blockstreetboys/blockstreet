205038a8ee018c7859e2794560da411c0f912a4d
Roots2D <cmccabe524@gmail.com>
2018-02-17 03:01:25 -0500

    Add spaceman module structure

---
+++ modules/spaceman/reactapp/src/component/shipped.jsx
@@ -0,0 +1,13 @@
+import React , { Component } from 'react';
+
+class Shipped extends Component {
+  render() {
+    return (
+      <div className='shipped'>
+          <button>Ship Goods</button>
+      </div>
+    );
+  }
+}
+
+export default Shipped;
