23d6d4825cf02bb7ec6bb50ba5f5587d19dd6091
Morgan Edwards <medwardsii@gmail.com>
2018-02-17 12:30:12 -0700

    Integrates ganache-cli

--- modules/spaceman/reactapp/src/web3Api/deploy.js
+++ modules/spaceman/reactapp/src/web3Api/deploy.js
@@ -1,8 +1,7 @@
 import { abi, bytecode } from './AstronautContract';
-const web3 = require('./web3config');
+const { web3 } = require('./web3config');
 
 const AstronautContract = web3.eth.contract(abi);
-let AstronautInstance;
 
 const deploy = (arbiterAddress, shipperAddress, astronautAddress, callback) => {
   AstronautContract.new(arbiterAddress, shipperAddress, {
