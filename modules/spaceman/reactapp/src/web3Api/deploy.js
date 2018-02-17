33e601419a826dca25c07e36468152d837d46e1b
Morgan Edwards <medwardsii@gmail.com>
2018-02-17 00:38:39 -0700

    Sets solidity version to 0.4.13.

--- modules/spaceman/reactapp/src/web3Api/deploy.js
+++ modules/spaceman/reactapp/src/web3Api/deploy.js
@@ -1,7 +1,11 @@
 import AstronautContract from './contract';
 
-const deploy = () => {
+const deploy = (arbiterAddress, shipperAddress, astronautAddress) => {
+  AstronautContract.new(arbiterAddress, shipperAddress, {
+    from: astronautAddress
+  });
   return new Promise(function(resolve, reject) {
-    
+
+      1000
   });
 };
