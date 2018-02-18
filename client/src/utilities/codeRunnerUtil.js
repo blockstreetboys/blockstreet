fb0019701082beeb9297e29c9bcdb6d8d1b1cd95
Daniel Nolan <DanNolan99@gmail.com>
2018-02-18 00:15:35 -0700

    simplifies coderunnerutil

--- client/src/utilities/codeRunnerUtil.js
+++ client/src/utilities/codeRunnerUtil.js
@@ -1,10 +1,6 @@
 const crUrl =  'https://codecast.qualified.io/relay';
 
 export const runCode = ({ script, tests, language, languageVersion, testFramework }, ref) => {
-  let resolve;
-  let reject;
-  let posts = [];
-
   ref.contentWindow.postMessage({
     method: 'run',
     data: {
@@ -15,17 +11,4 @@ export const runCode = ({ script, tests, language, languageVersion, testFramewor
       code: script
     }
   }, crUrl);
-
-  const promise = new Promise((_resolve, _reject) => {
-    resolve = _resolve;
-    reject = _reject;
-  });
-
-  const timeout = setTimeout(() => {
-    const post = posts.pop();
-    post.reject();
-  }, 2000);
-
-  posts.push({ promise, reject, resolve, timeout });
-  return promise;
 };
