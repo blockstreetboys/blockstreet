a5b85c59c453316a43386a4b11ce7d673d7bcc4c
Roots2D <cmccabe524@gmail.com>
2018-02-17 22:32:17 -0500

    Update to default state for module reducer

--- client/src/reducers/modules_reducer.js
+++ client/src/reducers/modules_reducer.js
@@ -1,6 +1,23 @@
 import { LOAD_SPACEMAN } from '../actions/module_actions';
 import { merge } from 'lodash';
 
+const defaultState = {
+  spaceman: {
+    apiStage: {
+      referenceSolution: "",
+      testCases: "",
+      title: ""
+    },
+    deploymentStage: {
+      preloaded: "",
+      referenceSolution: "",
+      testCases: "",
+      title: ""
+    },
+    solidityStages: []
+  }
+};
+
 const modulesReducer = (state = {}, action) => {
   switch (action.type) {
     case LOAD_SPACEMAN:
