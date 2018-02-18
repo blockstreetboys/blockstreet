50861cc35d203675e60d86e773b71afe8ded2943
Daniel Nolan <DanNolan99@gmail.com>
2018-02-18 08:29:25 -0700

    deploy works (to an extent)

--- client/src/components/completion.js
+++ client/src/components/completion.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 
 const mapStateToProps = state => {
   const { stages } = state.modules.spaceman;
---
+++ client/src/modules/spaceman/deploy.js
@@ -0,0 +1,24 @@
+import web3 from '../../utilities/web3Config';
+const data = require('./AstronautContract');
+const {abi, bytecode} = data;
+const AstronautContract = web3.eth.contract(abi);
+
+const deploy = (arbiterAddress, shipperAddress, astronautAddress, callback) => {
+  const contractData = AstronautContract.new.getData(arbiterAddress,
+    shipperAddress, {data: bytecode});
+    web3.eth.estimateGas({data: contractData}, (err, gas) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(`GAS: ${gas}`);
+    AstronautContract.new(arbiterAddress, shipperAddress, {
+      data: bytecode,
+      from: astronautAddress,
+      gas: gas
+    }, callback);
+  });
+
+};
+
+export default deploy;
